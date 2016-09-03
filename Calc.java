f79d4788b48d38deb20279da77447c431611dcb8
Divyesh <divyesh91@yahoo.com>
2016-09-02 19:54:38 -0700

    Calculator

---
+++ Calc.java
@@ -0,0 +1,28 @@
+package hacker.rank;
+
+import java.util.Scanner;
+
+class Add{
+	
+	public void Addition(){
+		
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the first number");
+	int a = s.nextInt();
+	System.out.println("Enter the second number");
+	int b = s.nextInt();
+	int c = +a+b;
+	s.close();
+	System.out.println("The sum is "+c);
+	}
+}
+
+public class Calc {
+	public static void main(String[] args) {
+		Add add = new Add();
+		add.Addition();
+
+		
+	}
+
+}
